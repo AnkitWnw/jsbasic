3233fa59cc78d5c2e5500f28a52e24f0cadd2c0b
AnkitWnw <138227964+AnkitWnw@users.noreply.github.com>
2024-11-13 06:23:43 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12545
+let accountEmail = "ankit@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+accountEmail = "ankit123@gmail.com"
+accountPassword = "12345678"
+accountCity = "Buxar"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
