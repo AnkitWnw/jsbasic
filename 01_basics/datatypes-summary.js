0a699d36b927fbde23ffbdc0d768ae9c8d681a37
AnkitWnw <138227964+AnkitWnw@users.noreply.github.com>
2024-11-13 08:30:20 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,53 @@
+// Types Of Primitive
+
+// 7 types
+
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. null
+// 5. undefined
+// 6. Symbol
+// 7. BigInt
+
+const score = 100;
+
+const scoreValue = 100.3;
+
+const isLoggedIn = false
+
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+
+const anotherId = Symbol('123')
+
+//console.table([score, scoreValue, isLoggedIn, outsideTemp, userEmail, (id === anotherId)])
+
+// Types Of Refrence (Non Primitive)
+
+// 3 types
+
+// 1. Array
+// 2. Objects
+// 3. Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj = {
+    name: "Ankit",
+    age: 27,
+}
+
+const myFunction = function() {
+    console.log("Hello Heros");
+}
+
+console.log(typeof myObj)
+    
+
+
+
+
