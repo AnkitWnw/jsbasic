226b23c6a35a6d4235e03145786992a1ee0969ea
AnkitWnw <138227964+AnkitWnw@users.noreply.github.com>
2024-11-13 06:50:42 +0000

    Conversion Operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = false
+
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+let isBooleanIn = 1
+
+let booleanIsLoggedIn = Boolean(isBooleanIn)
+
+//console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
