cd4e8e88f7a41ffba43679d7756b942fcbae6990
AnkitWnw <138227964+AnkitWnw@users.noreply.github.com>
2024-11-14 12:55:25 +0000

    strings example

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,12 @@
+const name = "Ankit"
+
+const repoCount = 50
+
+//console.log(name + repoCount + " value")
+
+console.log(`Hello, my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('Amit')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__)
