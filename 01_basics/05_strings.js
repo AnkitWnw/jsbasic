038fe526c6cdf20ca023e89ab5a476fe3cb05392
AnkitWnw <138227964+AnkitWnw@users.noreply.github.com>
2024-11-15 07:28:02 +0000

    another examples

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -4,9 +4,38 @@ const repoCount = 50
 
 //console.log(name + repoCount + " value")
 
-console.log(`Hello, my name is ${name} and my repo count is ${repoCount}`)
+//console.log(`Hello, my name is ${name} and my repo count is ${repoCount}`)
 
-const gameName = new String('Amit')
+const gameName = new String('Amit-Ankit-Ranjan ')
 
-console.log(gameName[0]);
-console.log(gameName.__proto__)
+// console.log(gameName[0]);
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+
+//console.log(gameName.charAt(1))
+
+const newString = gameName.substring(0, 3)
+
+//console.log(newString)
+
+const anotherString = gameName.slice(-2, 3)
+
+//console.log(anotherString)
+
+const newStringOne = "   Ankit    "
+
+// console.log(newStringOne)
+
+//console.log(newStringOne.trimEnd())
+
+//console.log(newStringOne.trimStart())
+
+const url ="https://ankit.com/ankit%20ranjan"
+
+//console.log(url.replace('%20', '-'))
+
+console.log(url.includes('amit'))
+
+console.log(gameName.split('-'))
