6c5c592f02f75bca5d0930c0b283a1611f112839
AnkitWnw <138227964+AnkitWnw@users.noreply.github.com>
2024-11-15 11:55:50 +0000

    date examples

---
+++ 01_basics/07_datesin_js.js
@@ -0,0 +1,35 @@
+//Date
+
+let myDate = new Date()
+
+// console.log(myDate)
+
+// console.log(myDate.toString())
+
+// console.log(myDate.toDateString())
+
+// console.log(myDate.toLocaleString())
+
+// console.log(typeof myDate)
+
+let myCreateDate = new Date(2024, 0, 15)
+
+//console.log(myCreateDate)
+
+let myCreateDates = new Date(2024, 0, 15, 5, 30)
+
+//console.log(myCreateDates.toLocaleString())
+
+//let myCreateDatess = new Date("2024-11-15")
+
+let myCreateDatess = new Date("11-15-2024")
+
+//console.log(myCreateDatess.toLocaleString())
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp)
+
+// console.log(myCreateDate.getTime());
+
+//console.log(Math.floor(Date.now()/1000))
