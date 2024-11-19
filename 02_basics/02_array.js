34ec6e913748c9ebbeefc408478461a5c248c064
AnkitWnw <138227964+AnkitWnw@users.noreply.github.com>
2024-11-19 08:14:43 +0000

    added more examples

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -23,4 +23,16 @@ const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
 
-console.log(real_another_array)
+//console.log(real_another_array)
+
+//console.log(Array.isArray("Ankit"))
+
+//console.log(Array.from("Ankit"))
+
+//console.log(Array.from({name: "Ankit"}))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3))
